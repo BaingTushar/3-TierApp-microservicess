1c4c51496308df70bceb9ae985563da96173c22e
Tushar_Baing <Baingtushar123@gmail.com>
2026-05-24 11:49:14 +0530

    Update index.js

--- Application-Code/backend/index.js
+++ Application-Code/backend/index.js
@@ -2,6 +2,7 @@ const tasks = require("./routes/tasks");
 const connection = require("./db");
 const cors = require("cors");
 const express = require("express");
+
 const app = express();
 
 connection();
@@ -9,11 +10,14 @@ connection();
 app.use(express.json());
 app.use(cors());
 
-app.get('/ok', (req, res) => {
-    res.status(200).send('ok')
-  })
+app.get("/ok", (req, res) => {
+  res.status(200).send("ok");
+});
 
 app.use("/api/tasks", tasks);
 
 const port = process.env.PORT || 3500;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+app.listen(port, () =>
+  console.log(`Listening on port ${port}...`)
+);
